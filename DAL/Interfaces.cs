8fbbb92a80a7a57d51c0c2100784510fc5779122
deasana <132469866+ddeasana@users.noreply.github.com>
2023-12-06 00:21:10 +0200

    initial commit

---
+++ DAL/Interfaces.cs
@@ -0,0 +1,31 @@
+﻿
+namespace DAL
+{
+    public interface IProvider<T> where T : class
+    {
+        public List<T> Load();
+        public void Save(List<T> listToSave);
+    }
+    //public interface IEntity
+    //{
+    //    public string LastName { get; set; }
+    //    public string[] Methods { get; }
+    //    public string ToString();
+    //}
+    interface IStudy
+    {
+        public string Study();
+    }
+    interface IManage
+    {
+        public string Manage();
+    }
+    interface ICook
+    {
+        public string Cook();
+    }
+    interface IPLayChess
+    {
+        public string PlayChess();
+    }
+}
